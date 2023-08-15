361328ae40a68d9e801b66351f3aa2d76866f43c
OlivetEkeh <ekehsons@gmail.com>
2023-08-15 06:44:54 -0700

    01

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - this prints the alphabeth in lower case
+ *
+ * Return: always 0 (Success)
+ */
+void print alphabet(void)
+{
+	int o;
+
+	for (o = 97; o <= 122; 0++)
+	{
+		_putchar(o);
+	}
+	_putchar('\n');
+}
