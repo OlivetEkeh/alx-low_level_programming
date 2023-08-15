7a9050190c22d660a926b3aed8027fb806213096
OlivetEkeh <ekehsons@gmail.com>
2023-08-15 08:23:55 -0700

    022

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -4,8 +4,8 @@
  * print_alphabet_x10 - this prints the alphabeth x10 in lower case
  *
  * Return: always 0 (Success)
+ *
  */
-
 void print_alphabet_x10(void)
 {
 
@@ -14,12 +14,11 @@ void print_alphabet_x10(void)
 	i = 0;
 	while (i < 10)
 	{
-	for (o = 'a' ; o <= 'z' ; o++)
-	{
-	_putchar(o);
+		for (o = 'a' ; o <= 'z' ; o++)
+		{
+			_putchar(o);
+		}
+		_putchar('\n');
+		i++;
 	}
-	_putchar('\n');
-	i++;
-	}
-
 }
