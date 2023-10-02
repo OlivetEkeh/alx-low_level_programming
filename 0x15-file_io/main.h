9987ba62ccd705378c25176b9ddc9a13d0732fbb
OlivetEkeh <OlivetEkeh>
2023-10-02 16:37:30 -0700

    appending text and creating a file

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,6 +7,9 @@
 #include <fcntl.h>
 #include <stddef.h>
 #include "main.h"
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <string.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
